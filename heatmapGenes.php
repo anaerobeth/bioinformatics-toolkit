362a3b3950d7ebfe567891a306a8f2f3f8fbad90
Beth Tenorio <betsky98@gmail.com>
2013-07-18 00:30:22 -0400

    Update heatmapGenes.php

--- heatmapGenes.php
+++ heatmapGenes.php
@@ -1,10 +1,10 @@
-<!-- This program generates a heatmap of esential genes in bacterial genome arranged according to gene number
+<!-- This program generates a heatmap of essential genes in bacterial genome arranged according to gene number
 
 Copyright 2012 Elizabeth Tenorio
 
 Steps:
 Upload sql tables to phpMyAdmin database pn_tnseq2013:  
-Assign colors to essential anh non essential genes
+Assign colors to essential and non essential genes
 Copy the result and add annotations
 
 -->
@@ -17,13 +17,15 @@ $databasepassword = "root";
 $con = @mysql_connect($databasehost,$databaseusername,$databasepassword) or die(mysql_error());
 @mysql_select_db($databasename) or die(mysql_error());
 
-//get heatcode etc
+//get heatcode data
 $query = "SELECT * FROM `essentialsmerged`";
 $result = @mysql_query($query);
 ?>
 <div style="width:1000px;">
 <?php
 
+//assign colors for gene categories: 
+//non-essential (darkblue), essential(red), not assigned(gray50), not present(black) 
 $count = 0;
 while ($row = mysql_fetch_assoc($result)) { 
 	$count++;  
@@ -47,6 +49,9 @@ while ($row = mysql_fetch_assoc($result)) {
 //    echo $code; 
 ?>
 
+<!-- Display the heatmap according to the described color scheme
+     Gif files for each color are included -->
+     
     <div style="float:left;width:20px;height:20px;background-image: url(<?php echo $color; ?>.gif)"></div>
 
     
