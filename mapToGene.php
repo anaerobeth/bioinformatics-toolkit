9ecf10ab7bfcd306b8e9e013f9ba5dc606acb98f
Beth Tenorio <anaerobeth@users.noreply.github.com>
2013-10-03 01:00:24 -0400

    Update mapToGene.php

--- mapToGene.php
+++ mapToGene.php
@@ -1,9 +1,9 @@
-<!-- This program maps the Illumina sequencing reads to the bacterial genome
+<!-- This program maps the Illumina sequencing reads to the bacterial genome of P. gingivalis
 
 Copyright 2012 Elizabeth Tenorio
 
 Steps:
-Upload w83 and 33277 sql tables to phpMyAdmin database pn_tnseq2013:  
+Upload W83 and 33277 sql tables to phpMyAdmin database pn_tnseq2013:  
 33277genes and w83genes
 fields include name, function, orientation, start, end
 
@@ -21,15 +21,14 @@ Copy the result, replace extra text with tabs then transfer to Excel
 
 
 <?php
-//program to loop through sample positions and match to gene name and function based on dansearch2.php
-
+// program to loop through sample positions and match to gene name and function based on dansearch2.php
 $databasename = "pn_tnseq2013";
 $databaseusername ="root";
 $databasepassword = "root";
 $con = @mysql_connect($databasehost,$databaseusername,$databasepassword) or die(mysql_error());
 @mysql_select_db($databasename) or die(mysql_error());
 
-//pull data from gene and sample tables        
+// pull data from gene and sample tables        
 $query = "SELECT * from w83genes";
 $result1 = @mysql_query($query);
 
@@ -37,14 +36,15 @@ $query2 = "SELECT * from tnreads527";
 $result2 = @mysql_query($query2);
          
 @mysql_close($con);
-//build arrays from gene and position tables
+
+// build arrays from gene and position tables
 while($r1[]=mysql_fetch_assoc($result1));
 while($r2[]=mysql_fetch_assoc($result2));
 
 $new = array();
-//$notfound = array();
 
-//get sample position and find the corresponding gene name and function by comparing the start and end positions of the gene
+// get sample position and find the corresponding gene name and function 
+// by comparing the start and end positions of the gene
 foreach ($r2 as $k => $v) {
 		$id 	= $r2[$k]['id'];
 		$read   = $r2[$k]['reads'];
@@ -56,23 +56,18 @@ foreach ($r2 as $k => $v) {
 		$gname 	= $r1[$kk]['name'];
 		$func	= $r1[$kk]['function'];
 
-		//build an array if a match is found then exit
+		// build an array if a match is found then exit
 		if ($pos >= $start && $pos <= $end) {
 					//		echo "found $id $gname $pos // $kk <br>";	
 			$new[$id] = array('position' => $pos, 'name' => $gname, 'function' => $func);
-			//$genes[$gname][] = $r2[$kk]['TotalCount'];
-// 			echo $pos;
-// 			echo "\t";
-// 			echo $gname;
-// 			echo "\n";
+
 			break;
 		}  
 
 	}		
 }
-//	echo $r2[$k]['Position'] . "<br>";
+
  echo "<pre>";
  print_r($new);
- //echo "<br>";
  echo "</pre>";
 ?>
